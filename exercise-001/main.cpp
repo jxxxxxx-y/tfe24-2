37f0699630d8946acd4b4a9627bfb208dd1963ac
jxxxxxx-y <beck.jan-felix-it24@it.dhbw-ravensburg.de>
2025-09-30 15:20:23 +0200

    completion of exercise-001

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -3,6 +3,9 @@
 
 #include "config.h"
 
+void changeValueByParameter (int value);
+void changeValueByReference (int& ref);
+
 auto main(int argc, char **argv) -> int
 {
 
@@ -11,9 +14,29 @@ auto main(int argc, char **argv) -> int
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", tfe24::PROJECT_NAME);
+    //fmt::print("Hello, {}!\n", tfe24::PROJECT_NAME);
 
-    /* INSERT YOUR CODE HERE */
+    // Part 1: References basics
+    int x = 10;
+    int& r = x; 
+    fmt::print("x = {}\n r = {}\n", x, r);
+    r = 42;
+    fmt::print("x = {}\n r = {}\n", x, r);
+
+    // Part 2: functions with references
+    int y = 5;
+    changeValueByParameter(y);
+    fmt::print("y after function: {}\n", y);
+    changeValueByReference(y);
+    fmt::print("y after function: {}\n", y);
 
     return 0; /* exit gracefully*/
+}
+void changeValueByParameter (int value){
+    value = 99;
+    fmt::print("y inside function: {}\n", value);
+}
+void changeValueByReference (int& ref){
+    ref = 77;
+    fmt::print("y inside function: {}\n", ref);
 }
