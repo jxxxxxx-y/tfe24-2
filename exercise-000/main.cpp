94e6aad8b9e4e1782c44fbb337e1219c10920a4c
jxxxxxx-y <beck.jan-felix-it24@it.dhbw-ravensburg.de>
2025-09-30 14:59:32 +0200

    Completion of exercise-000

--- exercise-000/main.cpp
+++ exercise-000/main.cpp
@@ -3,6 +3,16 @@
 
 #include "config.h"
 
+void changeValueByParameter (int value){
+        value = 99;
+        fmt::print("y inside function: {}\n", value);
+    };
+
+void changeValueByPointer (int *ptr){
+    *ptr = 77;
+    fmt::print("y inside function: {}\n", *ptr);
+};
+
 auto main(int argc, char **argv) -> int
 {
 
@@ -13,11 +23,36 @@ auto main(int argc, char **argv) -> int
      */
     //fmt::print("Hello, {}!\n", tfe24::PROJECT_NAME);
 
-    /* INSERT YOUR CODE HERE */
+
     // Part 1: Pointer-basics
     int x = 10;
     int *p = &x;
-    fmt::print("The value of x is: {}\n", *p);
-    fmt::print("Adress of x is: {}\n", fmt)
+    fmt::print("The value of x is: {}\n", x);
+    fmt::print("Adress of x is: {}\n", fmt::ptr(&x));
+    fmt::print("The value of p is: {}\n", *p);
+    fmt::print("Adress of p is: {}\n", fmt::ptr(&p));
+    *p = 42;
+    fmt::print("The new value of x is: {}\n", x);
+    fmt::print("The new value of p is: {}\n", *p);
+    fmt::print("\n");
+
+    // Part 2: Dynamic memory allocation
+    double* d = new double(3.1415);
+    fmt::print("The value of d is: {}\n", *d);
+    fmt::print("Adress of d is: {}\n", fmt::ptr(&d));
+    delete d;
+    fmt::print("\n");
+
+    // Part 3: functions with pointers
+    int y = 5;
+    changeValueByParameter (y); 
+    fmt::print("y after function: {}\n", y);
+    int *ptr = &y;
+    changeValueByPointer (&y);
+    fmt::print("y after function: {}\n", *ptr);
+
+
+
     return 0; /* exit gracefully*/
 }
+
