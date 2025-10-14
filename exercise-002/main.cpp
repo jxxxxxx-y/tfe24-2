2610ddf86306ac509b9958d7070f1b006a7c9122
Christian Ege <226893+christianege@users.noreply.github.com>
2025-10-14 14:26:06 +0200

    feat: add a first version to the solution 002

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -4,8 +4,27 @@
 #include "CLI/CLI.hpp"
 #include "config.h"
 
+// global variables in the various segments
+
+int bss;
+int bss1 = 0;
+
+int data = 23;
+int data1 = 29;
+
+const int rodata = 4711;
+const int rodata1 = 4712;
+
+void foo() { 
+    fmt::print("Hello from foo!\\n"); 
+}
+
 auto main(int argc, char **argv) -> int
 {
+
+    int localVar = 2;
+    int localVar1;
+
     /**
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
@@ -28,7 +47,22 @@ auto main(int argc, char **argv) -> int
      */
     fmt::print("Hello, {}!\n", app.get_name());
 
-    /* INSERT YOUR CODE HERE */
+    
+    fmt::println("The value of the variable bss: {} and its address {}", bss, fmt::ptr(&bss));
+    fmt::println("The value of the variable bss: {} and its address {}", bss1, fmt::ptr(&bss1));
+
+    fmt::println("The value of the variable data: {} and its address {}", data, fmt::ptr(&data));
+    fmt::println("The value of the variable data: {} and its address {}", data1, fmt::ptr(&data1));
+
+    fmt::println("The value of the variable rodata: {} and its address {}", rodata, fmt::ptr(&rodata));
+    fmt::println("The value of the variable rodata: {} and its address {}", rodata1, fmt::ptr(&rodata1));
+
+    fmt::println("------------------------------------------------------------------------------------------------");
+    fmt::println("The value of the variable localVar: {} and its address {}", localVar, fmt::ptr(&localVar));
+    fmt::println("The value of the variable localVar1: {} and its address {}", localVar1, fmt::ptr(&localVar1));
+
+    fmt::println("------------------------------------------------------------------------------------------------");
+    fmt::print("Adresse von foo: {}\\n", fmt::ptr(&foo));
 
     return 0; /* exit gracefully*/
 }
