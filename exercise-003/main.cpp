87002c078db60d1d6dbb2d5ef3b40a8ac19a33b4
jxxxxxx-y <beck.jan-felix-it24@it.dhbw-ravensburg.de>
2025-10-21 15:04:19 +0200

    completion of exercise-003

--- exercise-003/main.cpp
+++ exercise-003/main.cpp
@@ -1,5 +1,6 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
+#include <random>
 
 #include "CLI/CLI.hpp"
 #include "config.h"
@@ -32,12 +33,58 @@ auto main(int argc, char **argv) -> int
      */
     
     // Parameterraum
-    /*fmt::print("Hello {}\n", app.get_name());
-    fmt::print("counter value = {}\n", counter);*/
+    fmt::print("Hello {}\n", app.get_name());
+    fmt::print("counter value = {}\n", counter);
 
+    std::random_device r;
+    std::default_random_engine e1(r());
+    std::uniform_int_distribution<int> uniform_dist(1, 100);
     
+    //fmt::print("value for mean = {}\n", mean);
 
+    std::vector<int> rand_vector;
+    for (int i = 0; i < counter; i++)
+    {
+    int mean = uniform_dist(e1);
+    rand_vector.push_back(mean);
+    }
 
+    fmt::print("values:");
+    fmt::print("[");
+    for (int value : rand_vector){
+        fmt::print(" {}", value);
+    }
+    fmt::print("]");
+
+    std::sort(rand_vector.begin(), rand_vector.end());
+
+    fmt::print("\n");
+    fmt::print("values:");
+    fmt::print("[");
+    for (int value : rand_vector){
+        fmt::print(" {}", value);
+    }
+    fmt::print("]");
+
+    std::vector<int> speed_vec;
+    auto start = std::chrono::system_clock::now();
+    for (int i = 0; i < counter; i++)
+    {
+        speed_vec.push_back(uniform_dist(e1));
+    }
+    std::sort(speed_vec.begin(), speed_vec.end());
+    auto end = std::chrono::system_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+
+    fmt::print("\n");
+    fmt::print("Time taken for exercise: {}\n", elapsed);
+
+    fmt::print("values:");
+    fmt::print("[");
+    for (int value : speed_vec){
+        fmt::print(" {}", value);
+    }
+    fmt::print("]");
 
     return 0; /* exit gracefully*/
 }
