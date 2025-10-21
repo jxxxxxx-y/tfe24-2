e83103d2f7c18862581c393adcd20551b99c3007
jxxxxxx-y <beck.jan-felix-it24@it.dhbw-ravensburg.de>
2025-10-21 14:03:25 +0200

    exercise-003: Parameterspace

--- exercise-003/main.cpp
+++ exercise-003/main.cpp
@@ -4,16 +4,20 @@
 #include "CLI/CLI.hpp"
 #include "config.h"
 
+
 auto main(int argc, char **argv) -> int
 {
+    auto counter = 5;
     /**
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
      */
-    CLI::App app{PROJECT_NAME};
+    CLI::App app{"Strava for std::vectors"};
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+
+        app.add_option("-c,--count", counter, "An counter option")->default_val("20");
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
@@ -26,9 +30,14 @@ auto main(int argc, char **argv) -> int
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", app.get_name());
+    
+    // Parameterraum
+    /*fmt::print("Hello {}\n", app.get_name());
+    fmt::print("counter value = {}\n", counter);*/
 
-    /* INSERT YOUR CODE HERE */
+    
+
+
 
     return 0; /* exit gracefully*/
 }
